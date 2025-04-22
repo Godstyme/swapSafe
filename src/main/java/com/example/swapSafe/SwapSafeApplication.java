ec49d7c67ae868a1e06fe8c712acabe4b033d6f7
Godstime Onyibe <godstimeonyibe@gmail.com>
2025-04-22 15:57:32 +0100

    Put up a strcuture of the system

---
+++ src/main/java/com/example/swapSafe/SwapSafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.swapSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapSafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swapSafe/SwapSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swapSafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
